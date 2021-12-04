1b162ff09821ef228ae47574ac75fbd9fb94364f
Jp772 <jp772@njit.edu>
2021-12-04 14:09:23 -0500

    Added subtraction file input from subtraction.csv.

--- tests/subtraction.py
+++ tests/subtraction.py
@@ -1,11 +1,12 @@
 """Testing Subtraction"""
 from calc.calculations.subtraction import Subtraction
+import pandas as pd
 
 def test_calculation_subtraction():
-    """testing that our calculator has a static method for addition"""
-    #Arrange
-    mynumbers = (1.0,2.0)
-    subtraction = Subtraction(mynumbers)
-    #Act
-    #Assert
-    assert subtraction.get_result() == -3
+    """testing addition calculation from csv input"""
+    df = pd.read_csv("csvfiles/subtraction.csv")
+    print(df.head(5))
+    for i, j in df.iterrows():
+        sub = (j.col1, j.col2)
+        subtraction = Subtraction.create(sub)
+        assert subtraction.get_result() == df['result'][i]
