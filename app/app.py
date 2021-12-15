f3343a670845d8b19fa6191d1a71503b640cf2ae
Jp772 <jp772@njit.edu>
2021-12-14 21:53:59 -0500

    Remaining 3 pages have been added with links available via nav bar.

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import flash, render_template
 from app.controllers.index_controller import IndexController
 from app.controllers.calculator_controller import CalculatorController
-from app.controllers.project2_controller import Page1
+from app.controllers.project2_controller import Page1, Page2, Page3, Page4
 from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
@@ -33,16 +33,16 @@ def page1_get():
     return Page1.get()
 
 @app.route("/OOPTutorial", methods=["GET"])
-def page1_get():
+def page2_get():
     """Page 1 route"""
-    return Page1.get()
+    return Page2.get()
 
 @app.route("/AAATutorial", methods=["GET"])
-def page1_get():
+def page3_get():
     """Page 1 route"""
-    return Page1.get()
+    return Page3.get()
 
 @app.route("/SOLIDTutorial", methods=["GET"])
-def page1_get():
+def page4_get():
     """Page 1 route"""
-    return Page1.get()
+    return Page4.get()
